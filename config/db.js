e3ca95da0b2930cbbbdf764709e2eba0a4f2083f
m1moraru <164013436+m1moraru@users.noreply.github.com>
2026-04-25 12:54:36 +0100

    Fix database connection for Render

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
-import 'dotenv/config';
-import pkg from 'pg';
+import "dotenv/config";
+import pkg from "pg";
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5400,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
