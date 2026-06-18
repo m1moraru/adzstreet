6dba97cc4b63477e46bfb2efe6b9608e483ade90
m1moraru <164013436+m1moraru@users.noreply.github.com>
2026-06-18 21:03:31 +0100

    added account update function

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,11 +3,16 @@ import { requireAuth } from "../middleware/authMiddleware.js";
 import {
   deleteUserAccount,
   deleteProviderAccount,
+  updateAccountEmail,
+  updateAccountPassword,
 } from "../controllers/accountController.js";
 
 const router = express.Router();
 
 router.delete("/user", requireAuth, deleteUserAccount);
 router.delete("/provider", requireAuth, deleteProviderAccount);
+
+router.patch("/account/email", updateAccountEmail);
+router.patch("/account/password", updateAccountPassword);
 
 export default router;
